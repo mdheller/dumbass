5df2404f7684d9070bef164a424b584a9fae270d
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-07-09 20:38:26 +0800

    ok

--- r.js
+++ r.js
@@ -1,8 +1,11 @@
 "use strict";
 {
+  const XSS = `Possible XSS attack warning. Possible object forgery attempt detected. Codes do not match.`;
+  const OBJ = `Object properties don't work here.`;
   const LAST_ATTR_NAME = /\s+([\w-]+)\s*=\s*"?\s*$/;
   const NEW_TAG = /<[\w\-]+/;
   const parser = new DOMParser;
+  let currentKey = Math.random()+'';
 
   Object.assign(self,{R,render,fc});
 
@@ -12,8 +15,12 @@
     vals = vals.map( v => {
       if ( Array.isArray(v) && v.every(item => !!item.handlers && !!item.str) ) {
         return join(v) || '';
-      } else if ( typeof v == "object" && !!v && !(v.str && v.handlers) ) {
-        throw { error: `Object properties not allowed`, value: v };
+      } else if ( typeof v == "object" && !!v  ) {
+        if ( !(v.str && v.handlers) ) {
+          throw { error: OBJ, value: v };
+        } else {
+          return verify(v,currentKey) && v;
+        }
       } else return (v == null || v == undefined) ? '' : v;
     });
     let str = '';
@@ -67,13 +74,16 @@
       }
     }
     str += parts.shift();
-    return {str,handlers};
+    const o = {str,handlers};
+    o.code = sign(o,currentKey);
+    return o;
   }
 
   function render(r, root, {replace:replace = false} = {}) {
     if (Array.isArray(r) && r.every(val => !!val.str && !!val.handlers)) {
       r = join(r);
     }
+    verify(r,currentKey);
     let {str,handlers} = r;
     if ( replace ) {
       root.parentElement.replaceChild(fc(str),root);
@@ -96,7 +106,8 @@
   
   function join(rs) {
     const H = {};
-    const str = rs.map(({str,handlers}) => (Object.assign(H,handlers),str)).join('\n');
+    const str = rs.map(({str,handlers,code}) => (
+      verify({str,handlers,code},currentKey),Object.assign(H,handlers),str)).join('\n');
     if ( !! str ) {
       return {str,handlers:H};
     }
@@ -112,5 +123,45 @@
       return fragment;
     }
     return fragment.firstElementChild;
+  }
+
+  function sign({str,handlers},key) {
+    const val = str+':'+JSON.stringify(handlers,(k,v) => typeof v == "function" ? v+'' : v); 
+    const code = hash(key,val);
+    return code;
+  }
+
+  function verify({str,handlers,code},key) {
+    if ( sign({str,handlers},key) == code ) {
+      return true;
+    } 
+    throw {error: XSS};
+  }
+
+  function hash( key = '', str ) {
+    const s = str.length;
+    const m = bytes(key+str);
+    let a=new Float64Array(4);
+    a[0] = 1; 
+    a[2] = s ? Math.pow(s+1/s, 1/2) : 3;
+    a[3] = s ? Math.pow(s+1/s, 1/5) : 7;
+    m.forEach((x,i) => {
+      a[1] = (x+i+1)/a[3];
+      a[2] += a[0]/a[1]; a[2] = 1/a[2];
+      a[3] += x; a[3] = a[0]/a[3];
+      a[0] = a[1]+1;
+    });
+    a[2] *= Math.PI+a[3];
+    a[3] *= Math.E+a[2];  
+    const h = new Uint8Array(a.buffer).reduce((s,b) => s+b.toString(16).padStart(2,'0'));
+    return h;
+  }
+
+  function symbytes( sym ) {
+    return unescape(encodeURIComponent(sym)).split('').map(c => c.codePointAt(0));
+  }
+
+  function bytes(str) {
+    return Array.from(str).reduce((b,s) => (b.push(...symbytes(s)), b),[]);
   }
 }
