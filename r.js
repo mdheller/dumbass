bf8b05d00d7fb1d2e1637e848f33163dc87bf036
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-07-29 09:34:04 +0800

    New hotness working. Todoapp mostly works.

--- r.js
+++ r.js
@@ -2,10 +2,10 @@
   // now is not the time for optimization. 
   // That comes later.
 "use strict";
-  const DEBUG             = false;
+  const DEBUG             = true;
   const KEYMATCH          = / ?(key\d+) ?/gm;
   const KEYLEN            = 20;
-  const OURPROPS          = 'code,externals,frag,nodes,to,update,v';
+  const OURPROPS          = 'code,externals,nodes,to,update,v';
   const CODE              = ''+Math.random();
   const XSS               = () => `Possible XSS / object forgery attack detected. ` +
                             `Object value could not be verified.`;
@@ -22,6 +22,7 @@
   const INSERT            = () => `Error inserting template into DOM. ` +
                             `Position must be one of: ` +
                             `replace, beforeBegin, afterBegin, beforeEnd, innerHTML, afterEnd`;
+  const isKey             = v => typeof v === "object" &&  typeof v.key == "string";
   const cache = {};
 
   export {R};
@@ -29,8 +30,9 @@
   function R(p,...v) {
     v = v.map(parseVal);
 
+    const {key:instanceKey} = (v.find(isKey) || {});
     const cacheKey = p.join('<link rel=join>');
-    const {cached,firstCall} = isCached(cacheKey,v);
+    const {cached,firstCall} = isCached(cacheKey,v,instanceKey);
    
     if ( ! firstCall ) return cached;
 
@@ -46,10 +48,17 @@
     const frag = toDOM(str);
     const walker = document.createTreeWalker(frag, NodeFilter.SHOW_ALL);
 
-    do makeUpdaters({walker,vmap,externals}); while(walker.nextNode())
+    do {
+      makeUpdaters({walker,vmap,externals});
+    } while(walker.nextNode())
 
-    const retVal = cache[cacheKey] = {externals,frag,v:Object.values(vmap),to,
+    const retVal = {externals,v:Object.values(vmap),to,
       update,code:CODE,nodes:[...frag.childNodes]};
+    if ( !! instanceKey ) {
+      cache[cacheKey].instances[instanceKey] = retVal;
+    } else {
+      cache[cacheKey] = retVal;
+    }
     return retVal;
   }
 
@@ -60,12 +69,14 @@
         handleElementNode({node,vmap,externals});
       break;
       case Node.TEXT_NODE:
-        handleTextNode({node,vmap});
+        handleTextNode({node,vmap,externals});
+      break;
+      default:
       break;
     }
   }
 
-  function handleTextNode({node,vmap}) {
+  function handleTextNode({node,vmap,externals}) {
     const lengths = [];
     const text = node.wholeText; 
     let result;
@@ -73,105 +84,168 @@
       const {index} = result;
       const key = result[1];
       const val = vmap[key];
-      const replacer = makeTextNodeUpdater({node,index,lengths,valIndex:val.vi});
-      replacer(val.val);
+      const replacer = makeTextNodeUpdater({node,index,lengths,val,valIndex:val.vi});
+      externals.push(() => replacer(val.val));
       val.replacers.push( replacer );
     }
   }
 
-  function makeTextNodeUpdater({node,index,lengths,valIndex}) {
+  function makeTextNodeUpdater({node,index,lengths,valIndex,val}) {
     let oldNodes = [node];
+    let lastAnchor = node;
     return (newVal) => {
+      val.val = newVal;
       switch(typeof newVal) {
         case "object":
-          if ( sameNodes(oldNodes,newVal.nodes) ) return;
-          const anchorNode = oldNodes[0];
-          newVal.nodes.forEach(n => anchorNode.parentNode.insertBefore(n,anchorNode.nextSibling));
-          oldNodes.forEach(n => n.remove());
-          oldNodes = newVal.nodes;
+          if ( !! newVal.nodes.length ) {
+            newVal.nodes.forEach(n => lastAnchor.parentNode.insertBefore(n,lastAnchor.nextSibling));
+            const dn = diffNodes(oldNodes,newVal.nodes);
+            if ( dn.size ) {
+              const f = document.createDocumentFragment();
+              dn.forEach(n => f.appendChild(n));
+            }
+            lastAnchor = newVal.nodes[0];
+            oldNodes = newVal.nodes;
+          }
+          while ( newVal.externals.length ) {
+            newVal.externals.shift()(); 
+          } 
           break;
         default:
-          const lengthBefore = lengths.reduce((sum,x) => sum + (x || 0), 0);
+          const lengthBefore = lengths.slice(0,valIndex).reduce((sum,x) => sum + x, 0);
           node.nodeValue = newVal;
           lengths[valIndex] = newVal.length;
-            console.log(JSON.stringify({index,lengthBefore,lengths}));
           break;
       }
     };
   }
 
   function handleElementNode({node,vmap,externals}) {
-    const lengths = [];
-    const oldLengths = [];
     const attrs = [...node.attributes]; 
     attrs.forEach(({name,value}) => {
+      const lengths = [];
       let result;
+      while( result = KEYMATCH.exec(name) ) {
+        const {index} = result;
+        const key = result[1];
+        const val = vmap[key];
+        const replacer = makeAttributeUpdater(
+          {updateName:true,val,node,index,name,externals,lengths,valIndex:val.vi});
+        externals.push(() => replacer(val.val));
+        val.replacers.push( replacer );
+      }
       while( result = KEYMATCH.exec(value) ) {
         const {index} = result;
         const key = result[1];
         const val = vmap[key];
-        const replacer = makeAttributeUpdater({node,index,name,externals,lengths,oldLengths,valIndex:val.vi});
-        replacer(val.val);
+        const replacer = makeAttributeUpdater({val,node,index,name,externals,lengths,valIndex:val.vi});
+        externals.push(() => replacer(val.val));
         val.replacers.push( replacer );
       }
     });
   }
 
-  function makeAttributeUpdater({node,index,name,externals,lengths,oldLengths,valIndex}) {
+  function makeAttributeUpdater({
+    updateName: updateName = false,node,index,name,val,externals,lengths,oldLengths,valIndex}) {
     let oldVal = {length: KEYLEN};
-    let originalLengthBefore = Math.max(0,valIndex-1)*KEYLEN;
-    return (newVal) => {
-      switch(typeof newVal) {
-        case "function":
-          if ( name !== 'bond' ) {
-            if ( !! oldVal ) {
-              node.removeEventListener(name, oldVal);
-            }
-            node.addEventListener(name, newVal); 
-          } else {
-            if ( !! oldVal ) {
-              const index = externals.indexOf(oldVal);
-              if ( index >= 0 ) {
-                externals.splice(index,1);
+    let oldName = name;
+    if ( updateName ) {
+      return (newVal) => {
+        val.val = newVal;
+        switch(typeof newVal) {
+          default:
+            const attr = node.hasAttribute(oldName) ? oldName : ''
+            if ( attr !== newVal ) {
+              if ( !! attr ) {
+                node.removeAttribute(oldName);
               }
+              if ( !! newVal ) {
+                node.setAttribute(newVal,''); 
+              }
+              oldName = newVal;
             }
-            externals.push(() => newVal(node)); 
-          }
-          oldVal = newVal;
-        break;
-        default:
-          const attr = node.getAttribute(name);
-          if ( attr !== newVal ) {
-            const lengthBefore = lengths.reduce((sum,x,i) => i < valIndex ? sum + x : sum, 0) || 0;
-            lengths[valIndex] = newVal.length;
-
-            const correction = lengthBefore-originalLengthBefore;
-            const before = attr.slice(0,index+lengthBefore-originalLengthBefore);
-            const after = attr.slice(index+(lengthBefore-originalLengthBefore)+oldVal.length);
-
-            node.setAttribute(name,before + newVal + after);
-
+        }
+      };
+    } else {
+      return (newVal) => {
+        const originalLengthBefore = Object.keys(lengths.slice(0,valIndex)).length*KEYLEN;
+        lengths[valIndex] = newVal.length;
+        val.val = newVal;
+        switch(typeof newVal) {
+          case "function":
+            if ( name !== 'bond' ) {
+              if ( !! oldVal ) {
+                node.removeEventListener(name, oldVal);
+              }
+              node.addEventListener(name, newVal); 
+            } else {
+              if ( !! oldVal ) {
+                const index = externals.indexOf(oldVal);
+                if ( index >= 0 ) {
+                  externals.splice(index,1);
+                }
+              }
+              externals.push(() => newVal(node)); 
+            }
             oldVal = newVal;
-          }
-      }
-    };
+          break;
+          default:
+            const attr = node.getAttribute(name);
+            if ( attr !== newVal ) {
+              const lengthBefore = lengths.slice(0,valIndex).reduce((sum,x) => sum + x, 0);
+
+              const correction = lengthBefore-originalLengthBefore;
+              const before = attr.slice(0,index+correction);
+              const after = attr.slice(index+correction+oldVal.length);
+
+              //console.log(JSON.stringify({name,originalLengthBefore,lengthBefore,correction,index,attr,before,after,newVal,lengths,valIndex},null,2));
+              node.setAttribute(name,before + newVal + after);
+
+              oldVal = newVal;
+            }
+        }
+      };
+    }
   }
 
-  function isCached(cacheKey,v) {
+  function isCached(cacheKey,v,instanceKey) {
     let firstCall;
     let cached = cache[cacheKey];
     if ( cached == undefined ) {
       cached = cache[cacheKey] = {};
+      if ( !! instanceKey ) {
+        cached.instances = {};
+        cached = cached.instances[instanceKey] = {};
+      }
       firstCall = true;
     } else {
+      if ( !! instanceKey ) {
+        if ( ! cached.instances ) {
+          cached.instances = {};
+          firstCall = true;
+        } else {
+          cached = cached.instances[instanceKey];
+          if ( ! cached ) {
+            firstCall = true;
+          } else {
+            firstCall = false;
+          }
+        }
+      } else {
+        firstCall = false;
+      }
+    }
+    if ( ! firstCall ) {
       cached.update(v);
-      firstCall = false;
     }
     return {cached,firstCall};
   }
 
   function replaceVal(vmap) {
     return (val,vi) => {
+      if ( !! val.key ) {
+        return '';
+      }
       const k = (' key'+Math.random()+' ').replace('.','').padEnd(KEYLEN,'0').slice(0,KEYLEN);
       vmap[k.trim()] = {vi,val,replacers:[]};
       return k;
@@ -193,8 +267,9 @@
     const isVerified      = isObject          &&    verify(v);
     const isForgery       = onlyOurProps(v)   &&    !isVerified; 
 
-    if ( isFunc )        return v;
+    if ( isFunc )         return v;
     if ( isVerified )     return v;
+    if ( isKey(v) )       return v;
     if ( isGoodArray )    return join(v); 
     if ( isUnset )        die({error: UNSET()});
     if ( isForgery )      die({error: XSS()});
@@ -203,26 +278,36 @@
     return v+'';
   }
 
+  function itemIsFine(v) {
+    return onlyOurProps(v) && verify(v);
+  }
+
   function join(os) {
-    const bigFrag = document.createDocumentFragment();
-    os.forEach(o => bigFrag.appendChild(frag))
-    return {v:[],code:CODE,nodes:[...bigFrag.childNodes],frag:bigFrag,to,update};
+    const externals = [];
+    const bigNodes = [];
+    os.forEach(o => (externals.push(...o.externals),bigNodes.push(...o.nodes)));
+    const retVal = {v:[],code:CODE,nodes:bigNodes,to,update,externals};
+    return retVal;
   }
 
   function to(selector, position = 'replace') {
     const frag = document.createDocumentFragment();
     this.nodes.forEach(n => frag.appendChild(n));
-    const elem = document.querySelector(selector);
+    const elem = selector instanceof HTMLElement ? selector : document.querySelector(selector);
     try {
       MOVE[position](frag,elem);
     } catch(e) {
       die({error: INSERT()},e);
     }
-    this.externals.forEach(f => f());
+    while(this.externals.length) {
+      this.externals.shift()();
+    }
   }
 
-  function sameNodes(a,b) {
-    return a.every((node,index) => node === b[index])
+  function diffNodes(last,next) {
+    last = new Set(last);
+    next = new Set(next);
+    return new Set([...last].filter(n => !next.has(n)));
   }
 
   function update(newVals) {
@@ -230,7 +315,7 @@
   }
 
   function onlyOurProps(v) {
-    return OURPROPS === Object.keys(v||{}).sort().join(',');
+    return OURPROPS === Object.keys(v||{}).sort().filter(p => p !== 'instances').join(',');
   }
 
   function verify(v) {
