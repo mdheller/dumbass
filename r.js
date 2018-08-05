ebd64178b794cd8186ad3e68a367e383768607a5
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-08-05 11:22:23 +0800

    SSR improvement

--- r.js
+++ r.js
@@ -1,13 +1,13 @@
 "use strict";
   const DEBUG             = true;
   const BROWSER_SIDE      = (() => {try{ return self.DOMParser && true; } catch(e) { return false; }})();
-  // Shouldn't this match attrs that have text as well? currently it matches name=" but not name="asda 
   const LAST_ATTR_NAME    = /\s+([\w-]+)\s*=\s*"?\s*$/;
   const NEW_TAG           = /<\w+/g;
   const KEYMATCH          = / ?(?:<!\-\-)? ?(key\d+) ?(?:\-\->)? ?/gm;
   const KEYLEN            = 20;
   const OURPROPS          = 'code,externals,nodes,to,update,v';
   const CODE              = ''+Math.random();
+  const currentKey        = CODE;
   const XSS               = () => `Possible XSS / object forgery attack detected. ` +
                             `Object value could not be verified.`;
   const OBJ               = () => `Object values not allowed here.`;
@@ -29,30 +29,6 @@
   Object.assign(R,{s,skip,die,BROWSER_SIDE});
 
   export {R,X};
-
-  function X(p,...v) {
-    v = v.map(parseVal);
-
-    p = [...p]; 
-    const vmap = {};
-    const V = v.map(replaceVal(vmap));
-    const externals = [];
-    let str = '';
-
-    while( p.length > 1 ) str += p.shift() + V.shift();
-    str += p.shift();
-
-    const frag = toDOM(str);
-    const walker = document.createTreeWalker(frag, NodeFilter.SHOW_ALL);
-
-    do {
-      makeUpdaters({walker,vmap,externals});
-    } while(walker.nextNode())
-
-    const retVal = {externals,v:Object.values(vmap),to,
-      update,code:CODE,nodes:[...frag.childNodes]};
-    return retVal;
-  }
 
   function R(p,...v) {
     if ( ! BROWSER_SIDE ) return S(p,...v);
@@ -100,7 +76,7 @@
     const keyObj = vals.find( v => !!v && v.key !== undefined ) || {};
     const {key:key='singleton'} = keyObj;
 
-    vals = vals.map(SparseValue);
+    vals = vals.map(parseValue);
 
     while (parts.length > 1) {
       let part = parts.shift();
@@ -139,37 +115,91 @@
     return {str,handlers,to,code:CODE};
   }
 
-  function isCached(cacheKey,v,instanceKey) {
-    let firstCall;
-    let cached = cache[cacheKey];
-    if ( cached == undefined ) {
-      cached = cache[cacheKey] = {};
-      if ( !! instanceKey ) {
-        cached.instances = {};
-        cached = cached.instances[instanceKey] = {};
+  function safe (v) {
+    return String(v).replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/&/g,'&amp;').replace(/"/g,'&#34;').replace(/'/g,'&#39;');
+  }
+
+  function markInsertionPoint({str,lastNewTagIndex,lastTagName,hid}) {
+    const before = str.slice(0,lastNewTagIndex);
+    const after = str.slice(lastNewTagIndex);
+    return before + `<!--${hid}-->` + after;
+  }
+
+  function parseValue(v) {
+    if (Array.isArray(v) && v.every(item => !!item.handlers && !!item.str)) {
+      return Sjoin(v) || '';
+    } else if (typeof v === 'object' && !!v) {
+      if (!!v.str && !!v.handlers) {
+        return v;
       }
-      firstCall = true;
-    } else {
-      if ( !! instanceKey ) {
-        if ( ! cached.instances ) {
-          cached.instances = {};
-          firstCall = true;
-        } else {
-          cached = cached.instances[instanceKey];
-          if ( ! cached ) {
-            firstCall = true;
-          } else {
-            firstCall = false;
-          }
-        }
-      } else {
-        firstCall = false;
+      throw {error: OBJ(), value: v};
+    } else return v === null || v === undefined ? '' : v;
+  }
+
+  function hydrate(handlers) {
+    const hids = getHids(document);
+
+    Object.entries(handlers).forEach(
+      ([hid,nodeHandlers]) => activateNode(hids,{hid,nodeHandlers}));
+  }
+
+  function activateNode(hids,{hid,nodeHandlers}) {
+    const hidNode = hids[hid];
+    let node;
+
+    if (!!hidNode) {
+      node = hidNode.nextElementSibling;
+      hidNode.remove();
+    } else throw {error:MARKER(hid)}
+
+    const bondHandlers = [];
+
+    if (!!node && !!nodeHandlers) {
+      nodeHandlers.forEach(({eventName,handler}) => {
+        handler = revive(handler);
+        if ( eventName == 'bond' ) {
+          bondHandlers.push(()  => handler(node));
+        } else node.addEventListener(eventName,handler);
+      });
+    } else throw {error: HID(hid)} 
+
+    bondHandlers.forEach(f => f());
+  }
+
+  function getHids(parent) {
+    const walker = document.createTreeWalker(parent,NodeFilter.SHOW_COMMENT,{acceptNode: node => node.nodeType == Node.COMMENT_NODE && node.nodeValue.startsWith('hid_')});
+    const hids = {};
+    do{
+      const node = walker.currentNode;
+      if ( node.nodeType == Node.COMMENT_NODE && node.nodeValue.startsWith("hid_") ) {
+        hids[node.data] = node;
       }
-    }
-    if ( ! firstCall ) {
-      cached.update(v);
-    }
-    return {cached,firstCall};
+    } while(walker.nextNode());
+    return hids;
+  }
+
+  function X(p,...v) {
+    v = v.map(parseVal);
+
+    p = [...p]; 
+    const vmap = {};
+    const V = v.map(replaceVal(vmap));
+    const externals = [];
+    let str = '';
+
+    while( p.length > 1 ) str += p.shift() + V.shift();
+    str += p.shift();
+
+    const frag = toDOM(str);
+    const walker = document.createTreeWalker(frag, NodeFilter.SHOW_ALL);
+
+    do {
+      makeUpdaters({walker,vmap,externals});
+    } while(walker.nextNode())
+
+    const retVal = {externals,v:Object.values(vmap),to,
+      update,code:CODE,nodes:[...frag.childNodes]};
+    return retVal;
   }
 
   function makeUpdaters({walker,vmap,externals}) {
@@ -185,35 +215,6 @@
       break;
       default:
       break;
-    }
-  }
-
-  function markInsertionPoint({str,lastNewTagIndex,lastTagName,hid}) {
-    const before = str.slice(0,lastNewTagIndex);
-    const after = str.slice(lastNewTagIndex);
-    return before + `<!--${hid}-->` + after;
-  }
-
-  function SparseValue(v) {
-    if (Array.isArray(v) && v.every(item => !!item.handlers && !!item.str)) {
-      return Sjoin(v) || '';
-    } else if (typeof v === 'object' && !!v) {
-      if (!!v.str && !!v.handlers) {
-        return v;
-      }
-      throw {error: OBJ(), value: v};
-    } else return v === null || v === undefined ? '' : v;
-  }
-
-  function Sjoin(rs) {
-    const H = {};
-    const str = rs.map(({str,handlers,code}) => (
-        verify({str,handlers,code},CODE),Object.assign(H,handlers),str)).join('\n');
-
-    if (str) {
-      const o = {str,handlers:H};
-      o.code = CODE;
-      return o;
     }
   }
 
@@ -375,46 +376,43 @@
     }
   }
 
-  function hydrate(handlers) {
-    const hids = getHids(document);
-
-    Object.entries(handlers).forEach(
-      ([hid,nodeHandlers]) => activateNode(hids,{hid,nodeHandlers}));
-  }
-
-  function activateNode(hids,{hid,nodeHandlers}) {
-    const hidNode = hids[hid];
-    let node;
-
-    if (!!hidNode) {
-      node = hidNode.nextElementSibling;
-      hidNode.remove();
-    } else throw {error:MARKER(hid)}
-
-    const bondHandlers = [];
-
-    if (!!node && !!nodeHandlers) {
-      nodeHandlers.forEach(({eventName,handler}) => {
-        handler = revive(handler);
-        if ( eventName == 'bond' ) {
-          bondHandlers.push(()  => handler(node));
-        } else node.addEventListener(eventName,handler);
-      });
-    } else throw {error: HID(hid)} 
-
-    bondHandlers.forEach(f => f());
-  }
-
-  function getHids(parent) {
-    const walker = document.createTreeWalker(parent,NodeFilter.SHOW_COMMENT,{acceptNode: node => node.nodeType == Node.COMMENT_NODE && node.nodeValue.startsWith('hid_')});
-    const hids = {};
-    do{
-      const node = walker.currentNode;
-      if ( node.nodeType == Node.COMMENT_NODE && node.nodeValue.startsWith("hid_") ) {
-        hids[node.data] = node;
+  function isCached(cacheKey,v,instanceKey) {
+    let firstCall;
+    let cached = cache[cacheKey];
+    if ( cached == undefined ) {
+      cached = cache[cacheKey] = {};
+      if ( !! instanceKey ) {
+        cached.instances = {};
+        cached = cached.instances[instanceKey] = {};
       }
-    } while(walker.nextNode());
-    return hids;
+      firstCall = true;
+    } else {
+      if ( !! instanceKey ) {
+        if ( ! cached.instances ) {
+          cached.instances = {};
+          firstCall = true;
+        } else {
+          cached = cached.instances[instanceKey];
+          if ( ! cached ) {
+            firstCall = true;
+          } else {
+            firstCall = false;
+          }
+        }
+      } else {
+        firstCall = false;
+      }
+    }
+    if ( ! firstCall ) {
+      cached.update(v);
+    }
+    return {cached,firstCall};
+  }
+
+  function skip(str) {
+    str = (str || "")+'';
+    /* allow the thing to pass without replacement */
+    return { str, handlers: {}, code: currentKey };
   }
 
   function replaceVal(vmap) {
@@ -456,7 +454,11 @@
     if ( isForgery )      die({error: XSS()});
     if ( isObject )       die({error: OBJ()});
 
-    return safe(v+'');
+    return v+'';
+  }
+
+  function itemIsFine(v) {
+    return onlyOurProps(v) && verify(v);
   }
 
   function join(os) {
@@ -467,38 +469,55 @@
     return retVal;
   }
 
-  function to(location, position = 'replace') {
+  function Sjoin(rs) {
+    const H = {},
+      str = rs.map(({str,handlers,code}) => (
+        verify({str,handlers,code},currentKey),Object.assign(H,handlers),str)).join('\n');
+
+    if (str) {
+      const o = {str,handlers:H};
+      o.code = CODE;
+      return o;
+    }
+  }
+
+  function to(location, options) {
     if ( ! BROWSER_SIDE ) {
+      let {modules,scripts} = options || {};
+      modules = modules || [];
+      scripts = scripts || [];
       const {str,handlers} = this;
+      // notice this is a regular template literal
       const page = `
-        <html lang=en>
-          <!DOCTYPE html>
-          <meta charset=utf8>
-          <body> 
-            ${str}
-            <script>
-              const hydration = ${JSON.stringify(handlers,(k,v) => typeof v == "function" ? v.toString() : v)};
+        <!DOCTYPE html>
+        <meta charset=utf8>
+        <body> 
+          ${str}
+          <script>
+            const hydration = ${JSON.stringify(handlers,(k,v) => typeof v == "function" ? v.toString() : v)};
 
-              hydrate(hydration);
+            hydrate(hydration);
 
-              document.currentScript.remove();
+            document.currentScript.remove();
 
-              ${hydrate.toString()}
+            ${hydrate.toString()}
 
-              ${activateNode.toString()}
+            ${activateNode.toString()}
 
-              ${getHids.toString()}
-
-              function revive(fstr) {
-                const f = eval(fstr);
-                return f;
-              }
-            </script>
-          </body>
-        </html>
+            ${getHids.toString()}
+            
+            function revive(fstr) {
+              const f = eval(fstr);
+              return f;
+            }
+          </script>
+          ${modules.map(src => `<script type=module src=${src}></script>`)}
+          ${scripts.map(src => `<script src=${src}></script>`)}
+        </body>
       `;
       location.send(page); 
     } else {
+      const position = options || 'replace';
       const frag = document.createDocumentFragment();
       this.nodes.forEach(n => frag.appendChild(n));
       const elem = location instanceof Node ? location : document.querySelector(location);
@@ -523,26 +542,12 @@
     this.v.forEach(({vi,replacers}) => replacers.forEach(f => f(newVals[vi])));
   }
 
-  function itemIsFine(v) {
-    return onlyOurProps(v) && verify(v);
-  }
-
   function onlyOurProps(v) {
     return OURPROPS === Object.keys(v||{}).sort().filter(p => p !== 'instances').join(',');
   }
 
   function verify(v) {
     return CODE === v.code;
-  }
-
-  function safe (v) {
-    return String(v).replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/&/g,'&amp;').replace(/"/g,'&#34;').replace(/'/g,'&#39;');
-  }
-
-  function skip(str) {
-    str = (str || "")+'';
-    /* allow the thing to pass without replacement */
-    return { str, handlers: {}, code: CODE };
   }
 
   function die(msg,err) {
