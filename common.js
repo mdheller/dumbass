6317e48adb2d3fb61749a55c860d02407182d8ba
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-08-05 11:52:25 +0800

    SSR modularized

---
+++ common.js
@@ -0,0 +1,8 @@
+// common for all r submodules
+  export const CODE              = ''+Math.random();
+  export const BROWSER_SIDE      = (() => {try{ return self.DOMParser && true; } catch(e) { return false; }})();
+
+  export function safe (v) {
+    return String(v).replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/&/g,'&amp;').replace(/"/g,'&#34;').replace(/'/g,'&#39;');
+  }
+
--- r.js
+++ r.js
@@ -1,13 +1,11 @@
-"use strict";
+// r.js
+  import {S} from './ssr.js';
+  import {safe,CODE,BROWSER_SIDE} from './common.js';
+
   const DEBUG             = true;
-  const BROWSER_SIDE      = (() => {try{ return self.DOMParser && true; } catch(e) { return false; }})();
-  const LAST_ATTR_NAME    = /\s+([\w-]+)\s*=\s*"?\s*$/;
-  const NEW_TAG           = /<\w+/g;
   const KEYMATCH          = / ?(?:<!\-\-)? ?(key\d+) ?(?:\-\->)? ?/gm;
   const KEYLEN            = 20;
   const OURPROPS          = 'code,externals,nodes,to,update,v';
-  const CODE              = ''+Math.random();
-  const currentKey        = CODE;
   const XSS               = () => `Possible XSS / object forgery attack detected. ` +
                             `Object value could not be verified.`;
   const OBJ               = () => `Object values not allowed here.`;
@@ -67,117 +65,6 @@
     return retVal;
   }
 
-  function S(parts, ...vals) {
-    const handlers = {};
-    let hid, lastNewTagIndex, lastTagName, str = '';
-
-    parts = [...parts];
-
-    const keyObj = vals.find( v => !!v && v.key !== undefined ) || {};
-    const {key:key='singleton'} = keyObj;
-
-    vals = vals.map(parseValue);
-
-    while (parts.length > 1) {
-      let part = parts.shift();
-      let attrNameMatches = part.match(LAST_ATTR_NAME);
-      let newTagMatches = part.match(NEW_TAG);
-      let val = vals.shift();
-      if (newTagMatches) {
-        if ( handlers[hid] ) str = markInsertionPoint({str,lastNewTagIndex,lastTagName,hid});
-        hid = `hid_${Math.random()}`.replace('.','');
-        const lastTag = newTagMatches[newTagMatches.length-1];
-        lastNewTagIndex = part.lastIndexOf(lastTag) + str.length;
-        lastTagName = lastTag.slice(1);
-      }
-      if (typeof val === 'function') {
-        const attrName = attrNameMatches && attrNameMatches.length > 1
-            ? attrNameMatches[1].replace(/^on/,'').toLowerCase()
-            : false,
-          newPart = part.replace(attrNameMatches[0], '');
-        str += attrName ? newPart : part;
-        if ( !Array.isArray(handlers[hid]) ) handlers[hid] = [];
-        handlers[hid].push({eventName: attrName, handler: val});
-      } else if (!!val && !!val.handlers && typeof val.str == "string") {
-        Object.assign(handlers,val.handlers);
-        str += part;
-        val = val.str;
-        if (attrNameMatches) val = `${val}`;
-        str += val;
-      } else {
-        str += part;
-        str += attrNameMatches ? `${safe(val)}` : safe(val);
-      }
-    }
-    if ( handlers[hid] ) str = markInsertionPoint({str,lastNewTagIndex,lastTagName,hid});
-    str += parts.shift();
-
-    return {str,handlers,to,code:CODE};
-  }
-
-  function safe (v) {
-    return String(v).replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/&/g,'&amp;').replace(/"/g,'&#34;').replace(/'/g,'&#39;');
-  }
-
-  function markInsertionPoint({str,lastNewTagIndex,lastTagName,hid}) {
-    const before = str.slice(0,lastNewTagIndex);
-    const after = str.slice(lastNewTagIndex);
-    return before + `<!--${hid}-->` + after;
-  }
-
-  function parseValue(v) {
-    if (Array.isArray(v) && v.every(item => !!item.handlers && !!item.str)) {
-      return Sjoin(v) || '';
-    } else if (typeof v === 'object' && !!v) {
-      if (!!v.str && !!v.handlers) {
-        return v;
-      }
-      throw {error: OBJ(), value: v};
-    } else return v === null || v === undefined ? '' : v;
-  }
-
-  function hydrate(handlers) {
-    const hids = getHids(document);
-
-    Object.entries(handlers).forEach(
-      ([hid,nodeHandlers]) => activateNode(hids,{hid,nodeHandlers}));
-  }
-
-  function activateNode(hids,{hid,nodeHandlers}) {
-    const hidNode = hids[hid];
-    let node;
-
-    if (!!hidNode) {
-      node = hidNode.nextElementSibling;
-      hidNode.remove();
-    } else throw {error:MARKER(hid)}
-
-    const bondHandlers = [];
-
-    if (!!node && !!nodeHandlers) {
-      nodeHandlers.forEach(({eventName,handler}) => {
-        handler = revive(handler);
-        if ( eventName == 'bond' ) {
-          bondHandlers.push(()  => handler(node));
-        } else node.addEventListener(eventName,handler);
-      });
-    } else throw {error: HID(hid)} 
-
-    bondHandlers.forEach(f => f());
-  }
-
-  function getHids(parent) {
-    const walker = document.createTreeWalker(parent,NodeFilter.SHOW_COMMENT,{acceptNode: node => node.nodeType == Node.COMMENT_NODE && node.nodeValue.startsWith('hid_')});
-    const hids = {};
-    do{
-      const node = walker.currentNode;
-      if ( node.nodeType == Node.COMMENT_NODE && node.nodeValue.startsWith("hid_") ) {
-        hids[node.data] = node;
-      }
-    } while(walker.nextNode());
-    return hids;
-  }
-
   function X(p,...v) {
     v = v.map(parseVal);
 
@@ -202,8 +89,23 @@
     return retVal;
   }
 
+  function to(location, options) {
+    const position = options || 'replace';
+    const frag = document.createDocumentFragment();
+    this.nodes.forEach(n => frag.appendChild(n));
+    const elem = location instanceof Node ? location : document.querySelector(location);
+    try {
+      MOVE[position](frag,elem);
+    } catch(e) {
+      die({error: INSERT()},e);
+    }
+    while(this.externals.length) {
+      this.externals.shift()();
+    }
+  }
+
   function makeUpdaters({walker,vmap,externals}) {
-    //FIXME: If values are empty, things can fuck up.
+    //FIXME: If values are empty, things can break.
     let node = walker.currentNode;
     switch( node.nodeType ) {
       case Node.ELEMENT_NODE:
@@ -412,7 +314,7 @@
   function skip(str) {
     str = (str || "")+'';
     /* allow the thing to pass without replacement */
-    return { str, handlers: {}, code: currentKey };
+    return { str, handlers: {}, code: CODE };
   }
 
   function replaceVal(vmap) {
@@ -454,7 +356,7 @@
     if ( isForgery )      die({error: XSS()});
     if ( isObject )       die({error: OBJ()});
 
-    return v+'';
+    return safe(v+'');
   }
 
   function itemIsFine(v) {
@@ -467,69 +369,6 @@
     os.forEach(o => (externals.push(...o.externals),bigNodes.push(...o.nodes)));
     const retVal = {v:[],code:CODE,nodes:bigNodes,to,update,externals};
     return retVal;
-  }
-
-  function Sjoin(rs) {
-    const H = {},
-      str = rs.map(({str,handlers,code}) => (
-        verify({str,handlers,code},currentKey),Object.assign(H,handlers),str)).join('\n');
-
-    if (str) {
-      const o = {str,handlers:H};
-      o.code = CODE;
-      return o;
-    }
-  }
-
-  function to(location, options) {
-    if ( ! BROWSER_SIDE ) {
-      let {modules,scripts} = options || {};
-      modules = modules || [];
-      scripts = scripts || [];
-      const {str,handlers} = this;
-      // notice this is a regular template literal
-      const page = `
-        <!DOCTYPE html>
-        <meta charset=utf8>
-        <body> 
-          ${str}
-          <script>
-            const hydration = ${JSON.stringify(handlers,(k,v) => typeof v == "function" ? v.toString() : v)};
-
-            hydrate(hydration);
-
-            document.currentScript.remove();
-
-            ${hydrate.toString()}
-
-            ${activateNode.toString()}
-
-            ${getHids.toString()}
-            
-            function revive(fstr) {
-              const f = eval(fstr);
-              return f;
-            }
-          </script>
-          ${modules.map(src => `<script type=module src=${src}></script>`)}
-          ${scripts.map(src => `<script src=${src}></script>`)}
-        </body>
-      `;
-      location.send(page); 
-    } else {
-      const position = options || 'replace';
-      const frag = document.createDocumentFragment();
-      this.nodes.forEach(n => frag.appendChild(n));
-      const elem = location instanceof Node ? location : document.querySelector(location);
-      try {
-        MOVE[position](frag,elem);
-      } catch(e) {
-        die({error: INSERT()},e);
-      }
-      while(this.externals.length) {
-        this.externals.shift()();
-      }
-    }
   }
 
   function diffNodes(last,next) {
