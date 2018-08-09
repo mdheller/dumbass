d57af15391b16d0724c5a8c00f7103c7fa79477b
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-08-10 01:48:58 +0800

    patch version: T fully integrated

--- brutal.js
+++ brutal.js
@@ -237,244 +237,19 @@
       return i === null || i === undefined;
     }
 
-  const BROWSER_SIDE$2      = (() => {try{ return self.DOMParser && true; } catch(e) { return false; }})();
-
-    const BuiltIns$1 = [
-      Symbol, Boolean, Number, String, Object, Set, Map, WeakMap, WeakSet,
-      Uint8Array, Uint16Array, Uint32Array, Float32Array, Float64Array,
-      Int8Array, Int16Array, Int32Array, 
-      Uint8ClampedArray, 
-      ...(BROWSER_SIDE$2 ? [
-        Node,NodeList,Element,HTMLElement, Blob, ArrayBuffer,
-        FileList, Text, Document, DocumentFragment,
-        Error, File, Event, EventTarget, URL
-      ] : [ Buffer ])
-    ];
-
-    const isNone$1 = instance => instance == null || instance == undefined;
-
-    const typeCache$1 = new Map();
-
-    Object.assign(T$1, {check: check$1, sub: sub$1, verify: verify$2, validate: validate$1, def: def$1, defSub: defSub$1, defTuple: defTuple$1, defCollection: defCollection$1, defOr: defOr$1, option: option$1, defOption: defOption$1, or: or$1, guard: guard$1, errors: errors$1});
-
-    defineSpecials$1();
-    mapBuiltins$1();
-
-    function T$1(parts, ...vals) {
-      const cooked = vals.reduce((prev,cur,i) => prev+cur+parts[i+1], parts[0]);
-      const typeName = cooked;
-      if ( !typeCache$1.has(typeName) ) throw {error:`Cannot use type ${typeName} before it is defined.`};
-      return new Type$1(typeName);
-    }
-
-    function validate$1(type, instance) {
-      guardType$1(type);
-      guardExists$1(type);
-      let typeName = type.name;
-
-      const {spec,kind,verify} = typeCache$1.get(typeName);
-
-      const bigErrors = [];
-
-      switch(kind) {
-        case "def": 
-          let allValid = true;
-          if ( !! spec ) {
-            const keyPaths = Object.keys(spec);
-            allValid = !isNone$1(instance) && keyPaths.every(kp => {
-              const {resolved, errors:lookupErrors} = lookup$1(instance,kp);
-              bigErrors.push(...lookupErrors);
-              if ( lookupErrors.length ) return false;
-              const {valid, errors: validationErrors} = validate$1(spec[kp], resolved);
-              bigErrors.push(...validationErrors);
-              return valid;
-            });
-          }
-          let verified = true;
-          if ( !!verify ) {
-            try {
-              verified = verify(instance);
-            } catch(e) {
-              bigErrors.push(e);
-            }
-          }
-          return {valid: allValid && verified, errors: bigErrors}
-        case "defCollection":
-          const {valid:containerValid, errors:containerErrors} = validate$1(spec.container, instance);
-          bigErrors.push(...containerErrors);
-          let membersValid = true;
-          if ( containerValid ) {
-             membersValid= [...instance].every(member => {
-              const {valid, errors} = validate$1(spec.member, member);
-              bigErrors.push(...errors);
-              return valid;
-            });
-          }
-          return {valid:containerValid && membersValid, errors:bigErrors};
-        default: {
-          throw {error: `Checking for type kind ${kind} is not yet implemented.`}
-        }
-      }
-    }
-
-    function check$1(...args) {
-      return validate$1(...args).valid;
-    }
-
-    function lookup$1(obj, keyPath) {
-      if ( isNone$1(obj) ) throw {error:`Lookup requires a non-unset object.`};
-
-      if ( !keyPath ) throw {error: `keyPath must not be empty`};
-
-      const keys = keyPath.split(/\./g);
-      const pathComplete = [];
-      const errors = [];
-
-      let resolved = obj;
-
-      while(keys.length) {
-        const nextKey = keys.shift();
-        resolved = resolved[nextKey];
-        pathComplete.push(nextKey);
-        if ( keys.length && resolved == null || resolved == undefined ) {
-          errors.push( { error: `Lookup on key path ${keyPath} failed at ${pathComplete.join('.')}
-          when null or undefined was found.` });
-          break;
-        }
-      }
-      return {resolved,errors};
-    }
-
-    function option$1(type) {
-      return T$1`?${type.name}`;
-    }
-
-    function sub$1(type) {
-      return T$1`>${type.name}`;
-    }
-
-    function defSub$1(type, spec, {verify} = {}) {
-      guardType$1(type);
-      guardExists$1(type);
-      return def$1(`>${type.name}`, spec, {verify});
-    }
-
-    function exists$1(name) {
-      return typeCache$1.has(name);
-    }
-
-    function guardRedefinition$1(name) {
-      if ( exists$1(name) ) throw {error: `Type ${name} cannot be redefined.`};
-    }
-
-    function defOption$1(type) {
-      guardType$1(type);
-      const typeName = type.name;
-      return T$1.def(`?${typeName}`, null, {verify: i => isUnset$1(i) || T$1.check(type,i)});
-    }
-
-    function verify$2(...args) { return check$1(...args); }
-
-    function defCollection$1(name, {container, member}) {
-      if ( !name ) throw {error:`Type must be named.`}; 
-      if ( !container || !member ) throw {error:`Type must be specified.`};
-      guardRedefinition$1(name);
-
-      const kind = 'defCollection';
-      const spec = {kind, spec: { container, member}};
-      typeCache$1.set(name, spec);
-      return new Type$1(name);
-    }
-
-    function defTuple$1(name, {pattern}) {
-      if ( !name ) throw {error:`Type must be named.`}; 
-      if ( !pattern ) throw {error:`Type must be specified.`};
-      const kind = 'def';
-      const specObj = {};
-      pattern.forEach((type,key) => specObj[key] = type);
-      const spec = {kind, spec: specObj};
-      typeCache$1.set(name, spec);
-      return new Type$1(name);
-    }
-
-    function Type$1(name, mods = {}) {
-      if ( ! new.target ) throw {error:`Type with new only.`};
-      Object.defineProperty(this,'name', {get: () => name});
-      this.typeName = name;
-    }
-
-    Type$1.prototype.toString = function () {
-      return `${this.name} Type`;
-    };
-
-    function def$1(name, spec, {verify} = {}) {
-      if ( !name ) throw {error:`Type must be named.`}; 
-      guardRedefinition$1(name);
-
-      const kind = 'def';
-      typeCache$1.set(name, {spec,kind,verify});
-      return new Type$1(name);
-    }
-
-    function or$1(...types) { // anonymous standin for defOr
-      // could we do this with `name|name2|...` etc ?  we have to sort names so probably can
-      throw {error: `Or is not implemented yet.`};
-    }
-
-    function defOr$1(name, ...types) {
-      return T$1.def(name, null, {verify: i => types.some(t => check$1(t,i))});
-    }
-
-    function guard$1(type, instance) {
-      guardType$1(type);
-      guardExists$1(type);
-      if ( ! verify$2(type, instance) ) throw {error: `Type ${typeName} requested, but item is not of that type.`};
-    }
-
-    function guardType$1(t) {
-      if ( !(t instanceof Type$1) ) throw {error: `Type must be a valid Type object.`};
-    }
-
-    function guardExists$1(t) {
-      if ( ! exists$1(t.name) ) throw {error:`Type must exist. Type ${t.name} has not been defined.`};
-    }
-
-    function errors$1(...args) {
-      return validate$1(...args).errors;
-    }
-
-    function mapBuiltins$1() {
-      BuiltIns$1.forEach(t => def$1(t.name, null, {verify: i => i.constructor.name === t.name}));  
-      BuiltIns$1.forEach(t => defSub$1(T$1`${t.name}`, null, {verify: i => i instanceof t}));  
-    }
-
-    function defineSpecials$1() {
-      T$1.def(`Any`, null, {verify: i => true});
-      T$1.def(`Some`, null, {verify: i => !isUnset$1(i)});
-      T$1.def(`None`, null, {verify: i => isUnset$1(i)});
-      T$1.def(`Function`, null, {verify: i => i instanceof Function});
-      T$1.def(`Integer`, null, {verify: i => Number.isInteger(i)});
-      T$1.def(`Array`, null, {verify: i => Array.isArray(i)});
-      T$1.def(`Iterable`, null, {verify: i => i[Symbol.iterator] instanceof Function});
-    }
-
-    function isUnset$1(i) {
-      return i === null || i === undefined;
-    }
-
   // server side rendering 
 
     const LAST_ATTR_NAME    = /\s+([\w-]+)\s*=\s*"?\s*$/;
     const NEW_TAG           = /<\w+/g;
 
-    T$1.def('BrutalObject', {
-      str: T$1`String`,
-      handlers: T$1`Object`
+    T.def('SBrutalObject', {
+      str: T`String`,
+      handlers: T`Object`
     });
 
-    T$1.defCollection('BrutalArray', {
-      container: T$1`Array`,
-      member: T$1`BrutalObject`
+    T.defCollection('SBrutalArray', {
+      container: T`Array`,
+      member: T`SBrutalObject`
     });
 
     function S(parts, ...vals) {
@@ -483,7 +258,7 @@
 
       parts = [...parts];
 
-      const keyObj = vals.find( v => !!v && v.key !== undefined ) || {};
+      const keyObj = vals.find(v => T.check(T`Key`,v))|| {};
 
       vals = vals.map(SparseValue);
 
@@ -499,15 +274,15 @@
           lastNewTagIndex = part.lastIndexOf(lastTag) + str.length;
           lastTagName = lastTag.slice(1);
         }
-        if (T$1.check(T$1`Function`, val)) {
+        if (T.check(T`Function`, val)) {
           const attrName = attrNameMatches && attrNameMatches.length > 1
               ? attrNameMatches[1].replace(/^on/,'').toLowerCase()
               : false,
             newPart = part.replace(attrNameMatches[0], '');
           str += attrName ? newPart : part;
-          if ( T$1.check(T$1`Array`, handlers[hid]) ) handlers[hid] = [];
+          if ( T.check(T`Array`, handlers[hid]) ) handlers[hid] = [];
           handlers[hid].push({eventName: attrName, handler: val});
-        } else if (T$1.check(T$1`BrutalObject`, val)) {
+        } else if (T.check(T`SBrutalObject`, val)) {
           Object.assign(handlers,val.handlers);
           str += part;
           val = val.str;
@@ -536,7 +311,7 @@
       <body> 
         ${str}
         <script>
-          const hydration = ${JSON.stringify(handlers,(k,v) => T$1.check(T$1`Function`, v) ? v.toString() : v)};
+          const hydration = ${JSON.stringify(handlers,(k,v) => T.check(T`Function`, v) ? v.toString() : v)};
 
           hydrate(hydration);
 
@@ -567,14 +342,14 @@
     }
 
     function SparseValue(v) {
-      if ( T$1.check(T$1`BrutalArray`, v) ) {
+      if ( T.check(T`SBrutalArray`, v) ) {
         return Sjoin(v) || '';
-      } else if ( T$1.check(T$1`Object`, v) ) {
-        if ( T$1.check(T$1`BrutalObject`, v) ) {
+      } else if ( T.check(T`Object`, v) ) {
+        if ( T.check(T`SBrutalObject`, v) ) {
           return v;
         }
         throw {error: OBJ(), value: v};
-      } else T$1.check(T$1`None`, v) ? '' : v;
+      } else T.check(T`None`, v) ? '' : v;
     }
 
     function hydrate(handlers) {
@@ -649,7 +424,7 @@
       to: T`Function`,
       update: T`Function`,
       v: T`Array`
-    }, {verify: v => verify$3(v)});
+    }, {verify: v => verify$2(v)});
 
     T.defCollection('BrutalArray', {
       container: T`Array`,
@@ -1015,7 +790,7 @@
       this.v.forEach(({vi,replacers}) => replacers.forEach(f => f(newVals[vi])));
     }
 
-    function verify$3(v) {
+    function verify$2(v) {
       return CODE === v.code;
     }
 
