4452f14e853755b4ece75ca57b0ee82427d3c7c1
Cris <22254235+crislin2046@users.noreply.github.com>
2019-01-31 04:38:58 +0000

    Refers #57. Tests reveal that Brutal is normally safe. Calling safe() is never required.

--- common.js
+++ common.js
@@ -4,6 +4,7 @@
 
   export function safe (v) {
     if ( typeof v == "object" ) throw new TypeError(`safe() can only be called on a string, or primitive.`);  
+    return String(v);
     return String(v).replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&#34;').replace(/'/g,'&#39;');
   }
 
